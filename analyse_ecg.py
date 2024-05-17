4815bac42638c2dcd6558bfb46e1ec21a59398dc
AlexKometer <a.kometer@mci4me.at>
2024-05-17 15:20:01 +0200

    initial commit, copied from 2nd assignemnet

--- analyse_ecg.py
+++ analyse_ecg.py
@@ -1,7 +1,25 @@
+#import all required libraries and modules from read_pandas.py
 import scipy
-import read_pandas
-path = "data//"
-df = read_my_csv(path)
-peaks = scipy.signal.find_peaks(df, height=340)
-print("Hallo")
-print(peaks)
+from read_pandas import read_my_csv
+import numpy as np
+
+# Function to detect peaks in the ECG data, Output is an array of heart rates
+def peak_detection(path):
+    df = read_my_csv(path)
+    peaks = scipy.signal.find_peaks(df["Messwerte in mV"], height=350)
+    print("Peaks:", peaks)
+    peak_interval = np.diff(peaks[0])
+    peak_interval_seconds = peak_interval / 1000
+    hr = np.round(60 / peak_interval_seconds, 2)
+
+    return hr
+
+
+
+# Function to calculate heart rate statistics
+def calculate_hr_data(hr):
+    hr_max = hr.max()
+    hr_min = hr.min()
+    hr_mean = hr.mean()
+
+    return hr_max, hr_min, hr_mean
